616b3c80eed73a02fbb9bd5c93dfb4aa366446d6
Phenolah <phenolaha@gmail.com>
2023-09-10 16:59:53 -0700

    final

--- EndpointApp/views.py
+++ EndpointApp/views.py
@@ -1,16 +1,16 @@
 from django.http import JsonResponse
 from django.utils import timezone
-import datetime
+from datetime import datetime
 
 def api(request):
     slack_name = request.GET.get('slack_name', 'example_name')
     track = request.GET.get('track', 'backend')
     current_day = timezone.now().strftime('%A')
-    utc_time = timezone.now() + datetime.timedelta(minutes=2)
+    utc_time = datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%SZ")
     response = {
         "slack_name": slack_name,
         "current_day": current_day,
-        "utc_time": utc_time.strftime('%Y-%m-%dT%H:%M:%SZ'),
+        "utc_time": utc_time,
         "track": track,
         "github_file_url": "https://github.com/Phenolah/HNG/blob/main/EndpointApp/views.py",
         "github_repo_url": "https://github.com/Phenolah/HNG",
