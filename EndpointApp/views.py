a381e1be2318a1655ad8c2d6ff08872d2a02fbb2
Phenolah <phenolaha@gmail.com>
2023-09-10 16:25:39 -0700

    final

--- EndpointApp/views.py
+++ EndpointApp/views.py
@@ -4,7 +4,7 @@ import datetime
 
 def api(request):
     slack_name = request.GET.get('slack_name', 'example_name')
-    track = request.GET.get('track', 'Backend')
+    track = request.GET.get('track', 'backend')
     current_day = timezone.now().strftime('%A')
     utc_time = timezone.now() + datetime.timedelta(minutes=2)
     response = {
