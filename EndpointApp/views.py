f5cb9f9542de1c2e761330c8727840e8db899437
Phenolah <phenolaha@gmail.com>
2023-09-10 14:52:33 -0700

    final

--- EndpointApp/views.py
+++ EndpointApp/views.py
@@ -1,25 +1,3 @@
-'''from django.shortcuts import render
-from django.http import JsonResponse
-from django.utils import timezone
-import datetime
-
-# Create your views here.
-def api(request):
-    slack_name = request.GET.get('slack_name', 'Phenolah' )
-    track = request.GET.get('track', 'Backend')
-    current_day = timezone.now().strftime('%A')
-    utc_time = timezone.now() + datetime.timedelta(minutes=2)
-    response = {
-        "slack_name": slack_name,
-        "current_day": current_day,
-        "current_utc_day": utc_time.strftime('%Y-%m-%dT%H:%M:%SZ'),
-        "track": track,
-        "github_file_url":"https://github.com/Phenolah/HNG/blob/main/EndpointApp/views.py",
-        "github_repo_url": "https://github.com/Phenolah/HNG",
-        "status_code": 200
-    }
-    return JsonResponse(response)'''
-
 from django.http import JsonResponse
 from django.utils import timezone
 import datetime
