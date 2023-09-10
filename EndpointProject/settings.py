2b6ad7c120e480a5e3c5c787d5eb5a7ef84f9aa8
Phenolah <phenolaha@gmail.com>
2023-09-10 08:06:56 -0700

    req

--- EndpointProject/settings.py
+++ EndpointProject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-ux7py-v$ed$nzhns2l%%g*g&6_j%+a$g!^dn%ppnh+7qjnxcj@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', "127.0.0.1", '*']
 
 
 # Application definition
