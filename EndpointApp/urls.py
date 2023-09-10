5ebec79be8602f3ff22fc4243f07a263f401d2fe
Phenolah <phenolaha@gmail.com>
2023-09-10 13:51:10 -0700

    changes

--- EndpointApp/urls.py
+++ EndpointApp/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('api/', views.endpoint, name='endpoint'),
+    path('api', views.api, name='api'),
 ]
--- EndpointApp/views.py
+++ EndpointApp/views.py
@@ -4,20 +4,18 @@ from django.utils import timezone
 import datetime
 
 # Create your views here.
-def endpoint(request):
+def api(request):
     slack_name = request.GET.get('slack_name', 'Phenolah' )
-    track = request.GET.get('track', 'backend')
+    track = request.GET.get('track', 'Backend')
     current_day = timezone.now().strftime('%A')
-    current_utc_day = timezone.now() + datetime.timedelta(minutes=2)
+    utc_time = timezone.now() + datetime.timedelta(minutes=2)
     response = {
         "slack_name": slack_name,
         "current_day": current_day,
-        "current_utc_day": current_utc_day.strftime('%Y-%m-%dT%H:%M:%SZ'),
+        "current_utc_day": utc_time.strftime('%Y-%m-%dT%H:%M:%SZ'),
         "track": track,
         "github_file_url":"https://github.com/Phenolah/HNG/blob/main/EndpointApp/views.py",
         "github_repo_url": "https://github.com/Phenolah/HNG",
         "status_code": 200
     }
-
-
     return JsonResponse(response)
